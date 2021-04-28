d20870b1124f0229349fb4d32d58d269467035aa
Dax Borde <daxborde+github@gmail.com>
2021-04-27 20:09:27 -0400

    lower limit remove print statement

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Config():
     discriminator_method = 'Multi' # 'Multi' or 'Cond'
     load_pretrained_embed = False
     min_freq = 3
-    max_length = 100
+    max_length = 32
     embed_size = 256
     d_model = 256
     h = 4
