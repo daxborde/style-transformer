5b6ce961075289f9eeeabcdf5762507973ba47a3
Dax Borde <daxborde+github@gmail.com>
2021-04-28 20:59:03 -0400

    write raw outputs to a file

--- train.py
+++ train.py
@@ -402,6 +402,16 @@ def auto_eval(config, vocab, model_F, test_iters, global_step, temperature):
     # ppl_neg = evaluator.yelp_ppl(rev_output[0])
     # ppl_pos = evaluator.yelp_ppl(rev_output[1])
 
+    with open("rawout-0-neg.txt") as f:
+        f.write("gold, raw, rev")
+        for i in range(len(rev_output[0])):
+            f.write(f"{gold_text[0][i]}, {raw_output[0][i]}, {rev_output[0][i]}\n")
+
+    with open("rawout-1-pos.txt") as f:
+        f.write("gold, raw, rev")
+        for i in range(len(rev_output[1])):
+            f.write(f"{gold_text[1][i]}, {raw_output[1][i]}, {rev_output[1][i]}\n")
+
     for k in range(5):
         idx = np.random.randint(len(rev_output[0]))
         print('*' * 20, 'neg sample', '*' * 20)
