39b906eb70acd64dfd0411b5c3849e47503caa53
Dax Borde <daxborde+github@gmail.com>
2021-04-29 22:23:16 -0400

    Dev merge (#5)
    * Enron Data (#3)
    
    Add enron data loader and dataset
    
    Co-authored-by: Minh Pham <minhp4801@gmail.com>
    
    * Truncation, Rsync, Log Loss (#4)
    
    Truncate input to 100 characters. Include rsync call for saving to Google Drive when running on Colab. Logs the loss updates to a separate log file.
    
    * Loss Log Extract (#6)
    
    Extract loss log from odd ouput
    
    * Bleu (#7)
    
    insert logging for bleu calculation
    
    Co-authored-by: Minh Pham <minhp4801@gmail.com>

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import torch
 import time
-from data import load_dataset
+import os
+from data import load_enron
 from models import StyleTransformer, Discriminator
 from train import train, auto_eval
 
+def most_recent_path(rootpath, return_two=False):
+    if rootpath is None:
+        return None
+    if not s.path.exists(rootpath):
+        return None
+    l = os.listdir(rootpath)
+    if len(l) == 0:
+        return None
+    
+    l.sort()
+    ret = (os.path.join(rootpath, l[-1]), os.path.join(rootpath, l[-2]))
+    if not return_two:
+        ret = ret[0]
+    return ret
 
 class Config():
-    data_path = './data/yelp/'
+    data_path = './data/enronpa/'
     log_dir = 'runs/exp'
     save_path = './save'
     pretrained_embed_path = './embedding/'
@@ -14,7 +29,7 @@ class Config():
     discriminator_method = 'Multi' # 'Multi' or 'Cond'
     load_pretrained_embed = False
     min_freq = 3
-    max_length = 16
+    max_length = 32
     embed_size = 256
     d_model = 256
     h = 4
@@ -44,17 +59,23 @@ class Config():
     inp_rand_drop_fac = 0
     inp_drop_prob = 0
 
+    loss_log = './save/loss_log.txt'
 
 def main():
     config = Config()
-    train_iters, dev_iters, test_iters, vocab = load_dataset(config)
+    train_iters, test_iters, vocab = load_enron(config)
     print('Vocab size:', len(vocab))
     model_F = StyleTransformer(config, vocab).to(config.device)
     model_D = Discriminator(config, vocab).to(config.device)
     print(config.discriminator_method)
-    
-    train(config, vocab, model_F, model_D, train_iters, dev_iters, test_iters)
-    
 
+    # last_checkpoint = most_recent_path(most_recent_path(config.save_path), return_two=True)
+    # if last_checkpoint:
+    #     print(last_checkpoint)
+    #     model_D.load_state_dict(torch.load(last_checkpoint[1]))
+    #     model_F.load_state_dict(torch.load(last_checkpoint[0]))
+
+    train(config, vocab, model_F, model_D, train_iters, test_iters)
+    
 if __name__ == '__main__':
     main()
